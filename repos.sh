f1d840c4e8a430c3bdbd541dc4f8d07a3d803020
Andres Pedraza <andrespedca@gmail.com>
2025-04-01 20:44:32 -0500

    fixed issue repos.sh

--- repos.sh
+++ repos.sh
@@ -7,7 +7,7 @@ dnf update -y
 dnf install distribution-gpg-keys -y
 
 #wait rpm fusion to be available for 10 release
-RPM Fusion (free packages)
+#RPM Fusion (free packages)
 rpmkeys --import /usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-free-el-$(rpm -E %rhel)
 # RPM Fusion (nonfree packages)
 rpmkeys --import /usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-nonfree-el-$(rpm -E %rhel)
