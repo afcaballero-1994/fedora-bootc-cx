efc595ddefe7a58cfbbab887c8e5f7b54e365484
Andres Pedraza <andrespedca@gmail.com>
2025-04-01 20:52:13 -0500

    added -y flag dnf

--- repos.sh
+++ repos.sh
@@ -6,6 +6,6 @@ dnf update -y
 
 dnf install distribution-gpg-keys -y
 
-dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-dnf config-manager setopt fedora-cisco-openh264.enabled=1
+dnf config-manager -y setopt fedora-cisco-openh264.enabled=1
