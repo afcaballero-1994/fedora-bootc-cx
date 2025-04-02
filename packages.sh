f614480ff39ac160a515530feab7b9b4711f32d1
Andres Pedraza <andrespedca@gmail.com>
2025-04-01 21:37:45 -0500

    install less packages
    the image size may be too big

--- packages.sh
+++ packages.sh
@@ -4,5 +4,5 @@ set -ouex pipefail
 
 dnf install -y fuse selinux-policy git distrobox neovim emacs ffmpeg
 
-dnf group install -y kde-desktop kde-apps kde-pim kde-media libreoffice
+dnf group install -y kde-desktop
 systemctl enable sddm.service
