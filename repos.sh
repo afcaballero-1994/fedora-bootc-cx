3d9a582032407eb21dbb3809c9323eeddf3b5c01
Andres Pedraza <andrespedca@gmail.com>
2025-04-01 20:48:25 -0500

    fixed issue repos.sh

--- repos.sh
+++ repos.sh
@@ -6,11 +6,6 @@ dnf update -y
 
 dnf install distribution-gpg-keys -y
 
-#wait rpm fusion to be available for 10 release
-#RPM Fusion (free packages)
-rpmkeys --import /usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-free-el-$(rpm -E %rhel)
-# RPM Fusion (nonfree packages)
-rpmkeys --import /usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-nonfree-el-$(rpm -E %rhel)
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-dnf --setopt=localpkg_gpgcheck=1 install  \
-https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm -y
+dnf config-manager setopt fedora-cisco-openh264.enabled=1
